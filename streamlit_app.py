e8b12a70be5caa1706bafadeaebcc472305ded78
Tanish Patel <tanishpatel0106@gmail.com>
2023-07-04 12:02:53 +0530

    RegEx Patterns changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,27 +19,31 @@ class CTokenizer:
             '<<=', '>>=', '&=', '^=', '|=', ','
         ]
 
-        # Build the regular expression pattern
-        self.pattern = r'({})|\b([a-zA-Z_][a-zA-Z0-9_]*)\b|([0-9]+)|"([^"\\]*(?:\\.[^"\\]*)*)"|([^\w\s])'.format(
-            '|'.join(map(re.escape, self.keywords + self.punctuators))
-        )
+        # Build the regular expression patterns
+        self.keyword_pattern = r'\b(?:{})\b'.format('|'.join(map(re.escape, self.keywords)))
+        self.identifier_pattern = r'\b([a-zA-Z_][a-zA-Z0-9_]*)\b'
+        self.constant_pattern = r'\b([0-9]+)\b'
+        self.string_literal_pattern = r'"([^"\\]*(?:\\.[^"\\]*)*)"'
+        self.punctuator_pattern = r'({})'.format('|'.join(map(re.escape, self.punctuators)))
 
     def tokenize_c_program(self, c_program):
         tokens = []
         # Remove comments by replacing them with whitespace
         c_program = re.sub(r'(\/\*[\s\S]*?\*\/|\/\/.*)', lambda m: ' ' * len(m.group()), c_program)
-        for match in re.finditer(self.pattern, c_program):
-            keyword, identifier, constant, string_literal, punctuator = match.groups()
-            if keyword:
-                tokens.append({'Type': 'KEYWORD', 'Value': keyword})
-            elif identifier:
-                tokens.append({'Type': 'IDENTIFIER', 'Value': identifier})
-            elif constant:
-                tokens.append({'Type': 'CONSTANT', 'Value': constant})
-            elif string_literal:
-                tokens.append({'Type': 'STRING_LITERAL', 'Value': string_literal})
-            elif punctuator:
-                tokens.append({'Type': 'PUNCTUATOR', 'Value': punctuator})
+        
+        # Tokenize the program
+        patterns = [
+            (self.keyword_pattern, 'KEYWORD'),
+            (self.identifier_pattern, 'IDENTIFIER'),
+            (self.constant_pattern, 'CONSTANT'),
+            (self.string_literal_pattern, 'STRING_LITERAL'),
+            (self.punctuator_pattern, 'PUNCTUATOR')
+        ]
+        
+        for pattern, token_type in patterns:
+            for match in re.finditer(pattern, c_program):
+                tokens.append({'Type': token_type, 'Value': match.group()})
+                
         return tokens
 
 
